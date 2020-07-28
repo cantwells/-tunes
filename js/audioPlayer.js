2e6663a5ace303b4638140f5d4de062d30869bbc
cantwell <rjazantsev@gmail.com>
2020-07-28 10:42:21 +0300

    import external js files

---
+++ js/audioPlayer.js
@@ -0,0 +1,3 @@
+export const audioPlayerInit = () => {
+    console.log('audio init');
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+//Импортируем внешние файлы
+import { videoPlayerInit } from './videoPlayer.js';
+import { audioPlayerInit } from './audioPlayer.js';
+import { radioPlayerInit } from './radioPlayer.js';
