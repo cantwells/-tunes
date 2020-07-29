7936cbcab7c7b67731e0e1baaec3809de4cdfa5b
cantwell <rjazantsev@gmail.com>
2020-07-29 16:25:20 +0300

    includes dom elements

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,3 +1,11 @@
 export const videoPlayerInit = () => {
-    console.log('video init');
+    //=============================Получаем элемента=============================================
+    const videoPlayer = document.querySelector('.video-player'), //контейнер с видео
+        videoButtonPlay = document.querySelector('.video-button__play'), //кнопка play
+        videoButtonStop = document.querySelector('.video-button__stop'), //кнопка stop
+        videoTimePassed = document.querySelector('.video-time__passed'), //время сколько пройдено
+        videoTimeTotal = document.querySelector('.video-time__total'), //полное вреся
+        videoProgress = document.querySelector('.video-progress'); //прогресс бар 
+
+
 }
