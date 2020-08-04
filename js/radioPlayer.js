f3186722c7122f6067077378c0a1e05f42c14357
cantwell <rjazantsev@gmail.com>
2020-08-04 17:03:16 +0300

    get html elements

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,3 +1,12 @@
 export const radioPlayerInit = () => {
-    console.log('radio init');
+
+    //===========================Получение элементов=======================================================
+    const radioNavigation = document.querySelector('.radio-navigation'), //форма с радиостанциями
+        radioСoverImg = document.querySelector('.radio-cover__img'), //центральная картинка 
+        radioHeaderBig = document.querySelector('.radio-header__big'), //Заголовок с названием радиостанции
+        radioItems = document.querySelectorAll('.radio-item'), //все радиостанции
+        radioStop = document.querySelector('.radio-stop'); //кнопка включения
+
+    radioStop.disabled = true; //делаем не активной кнопку play
+    const audio = new Audio(); //получаем экземпляр класса Audio
 }
