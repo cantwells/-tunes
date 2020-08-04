a00494931874748cca69c5c83933aa3b17d0d776
cantwell <rjazantsev@gmail.com>
2020-08-04 17:05:49 +0300

    playing radio

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -9,4 +9,47 @@ export const radioPlayerInit = () => {
 
     radioStop.disabled = true; //делаем не активной кнопку play
     const audio = new Audio(); //получаем экземпляр класса Audio
+
+    //==================================Функции============================================================
+    //Отметить радиостанцию
+    const selectItem = chosen => {
+            //убираем у всех класс для выделения
+            radioItems.forEach(item => item.classList.remove('select'));
+            chosen.classList.add('select');
+        }
+        //меняем вид иконки play и запускаем или останавливаем анимацию
+    const togglePlay = () => {
+        if (audio.paused) {
+            radioStop.classList.remove('fa-stop');
+            radioStop.classList.add('fa-play');
+            radioСoverImg.style.animationPlayState = 'paused';
+        } else {
+            radioStop.classList.remove('fa-play');
+            radioStop.classList.add('fa-stop');
+            radioСoverImg.style.animationPlayState = 'running';
+        }
+    }
+
+    //==================================Событие============================================================
+    //Вешаем событие на форму и с помощью делегирования ищем инпут
+    radioNavigation.addEventListener('click', event => {
+        const target = event.target;
+        if (target.tagName === 'INPUT') {
+            //подставляем в свойство src адрес потокового радио, который находится в дата-сет атрибуте
+            audio.src = target.dataset.radioStation;
+            //запускаем радио
+            audio.play();
+            //делаем кнопку play активной
+            radioStop.disabled = false;
+            //получаем родителя (карточку с радиостанцией)
+            const parent = target.closest('.radio-item');
+            selectItem(parent);
+            //подставляем название станции в заголовок
+            radioHeaderBig.textContent = parent.querySelector('.radio-name').textContent;
+            //подставляем картинку
+            radioСoverImg.src = parent.querySelector('.radio-img').src;
+            togglePlay();
+        }
+
+    })
 }
