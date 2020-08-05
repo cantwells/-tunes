05f57d64d20a256eca8fbc6e71e7991f2d6b2958
cantwell <rjazantsev@gmail.com>
2020-08-05 13:59:25 +0300

    beuatify code

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,7 +1,8 @@
 export const radioPlayerInit = () => {
 
     //===========================Получение элементов=======================================================
-    const radioNavigation = document.querySelector('.radio-navigation'), //форма с радиостанциями
+    const radio = document.querySelector('.radio'), //вся вкладка радио
+        radioNavigation = document.querySelector('.radio-navigation'), //форма с радиостанциями
         radioСoverImg = document.querySelector('.radio-cover__img'), //центральная картинка 
         radioHeaderBig = document.querySelector('.radio-header__big'), //Заголовок с названием радиостанции
         radioItems = document.querySelectorAll('.radio-item'), //все радиостанции
@@ -9,6 +10,8 @@ export const radioPlayerInit = () => {
 
     radioStop.disabled = true; //делаем не активной кнопку play
     const audio = new Audio(); //получаем экземпляр класса Audio
+    audio.type = 'audio/aac'; //указываем тип
+
 
     //==================================Функции============================================================
     //Отметить радиостанцию
@@ -20,21 +23,35 @@ export const radioPlayerInit = () => {
         //меняем вид иконки play и запускаем или останавливаем анимацию
     const togglePlay = () => {
         if (audio.paused) {
+            //меняем ярлычки
             radioStop.classList.remove('fa-stop');
             radioStop.classList.add('fa-play');
-            radioСoverImg.style.animationPlayState = 'paused';
+            // radioСoverImg.style.animationPlayState = 'paused';
+            //отключаем анимацию
+            radio.classList.remove('play');
         } else {
             radioStop.classList.remove('fa-play');
             radioStop.classList.add('fa-stop');
-            radioСoverImg.style.animationPlayState = 'running';
+            // radioСoverImg.style.animationPlayState = 'running';
+            //включаем анимацию
+            radio.classList.add('play');
+        }
+    }
+
+    const changeIcon = () => {
+        if (audio.muted || !audio.volume) {
+            radioButton.classList.remove('fa-volume-up');
+            radioButton.classList.add('fa-volume-off');
+        } else {
+            radioButton.classList.remove('fa-volume-off');
+            radioButton.classList.add('fa-volume-up');
         }
     }
 
     //==================================Событие============================================================
     //Вешаем событие на форму и с помощью делегирования ищем инпут
-    radioNavigation.addEventListener('click', event => {
-        const target = event.target;
-        if (target.tagName === 'INPUT') {
+    radioNavigation.addEventListener('change', event => {
+            const target = event.target;
             //подставляем в свойство src адрес потокового радио, который находится в дата-сет атрибуте
             audio.src = target.dataset.radioStation;
             //запускаем радио
@@ -49,9 +66,8 @@ export const radioPlayerInit = () => {
             //подставляем картинку
             radioСoverImg.src = parent.querySelector('.radio-img').src;
             togglePlay();
-        }
-
-    })
+        })
+        //Кнопка Play 
     radioStop.addEventListener('click', event => {
         //запускаем или останавливаем радио
         audio.paused ? audio.play() : audio.pause();
